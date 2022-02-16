0dc98d3745850d07a878e887ff71185dae107489
AlexandreCMoraes <alexcmoraes@hotmail.com>
2022-02-16 20:40:31 -0300

    add tratamento de erros em leitura de peso e altura, mas ainda dando problema nas condicoes

--- src/imc/ImcLeitura.java
+++ src/imc/ImcLeitura.java
@@ -15,14 +15,37 @@ public class ImcLeitura extends Imc{
         entrada = new Scanner(System.in);
     }
 
+    public double LerDouble(String texto){
+        boolean continuarLeitura = true;
+        //feito ate dar um double valido
+        double valor = 0;
+        while(continuarLeitura){
+            System.out.println(texto);
+            try {
+                valor = Double.parseDouble(entrada.nextLine());
+                //le a entrada (usuario) e atribui/converte o double(digitado) no valor
+                continuarLeitura = false;
+            }
+            catch (NumberFormatException e){
+                System.out.println("Entrada com valor inválido. Digite apenas números");
+            }
+        }
+        return valor;
+    }
+
     public void LerPeso() {
         //vai pegar o dado de peso
-        System.out.println("Entre com seu peso: ");
-        this.setPeso(entrada.nextDouble());
+        //System.out.println("Entre com seu peso: ");
+        //this.setPeso(entrada.nextDouble());
+        //feito abaixo com tratamento de erro
+        this.setPeso(this.LerDouble("Entre com seu peso: "));
     }
 
     public void lerAltura() {
-        System.out.println("Entre com sua altura: ");
-        this.setAltura(entrada.nextDouble());
+        //vai pegar o dado da altura
+        //System.out.println("Entre com sua altura: ");
+        //this.setAltura(entrada.nextDouble());
+        //feito abaixo com tratamento de erro
+        this.setAltura(this.LerDouble("Entre com sua altura em centímetros"));
     }
 }
